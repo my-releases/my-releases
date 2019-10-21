974075f18299ee64787252b87d107a2390c46a14
Omer Mohamed Ali <om@anexia.at>
2019-10-21 23:45:00 +0200

    Fix analytics code

--- resources/views/analytics.php
+++ resources/views/analytics.php
@@ -1,11 +1,11 @@
 <?php if(!$isDev): ?>
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-149257559-1"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-150612513-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
-        gtag('config', 'UA-149257559-1');
+        gtag('config', 'UA-150612513-1');
     </script>
 <?php endif; ?>
